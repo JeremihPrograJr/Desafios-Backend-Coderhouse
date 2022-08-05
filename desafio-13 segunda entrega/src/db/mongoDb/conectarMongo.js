8da49e961cdcc08e425f89e2df10eea2c21fa46d
JeremiasProgJr <48654253+JeremiasProgJr@users.noreply.github.com>
2022-08-04 21:00:18 -0400

    conectando a mongo en la nube

--- desafio-13 segunda entrega/src/db/mongoDb/conectarMongo.js
+++ desafio-13 segunda entrega/src/db/mongoDb/conectarMongo.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 const url = require('../../config/database');
 
-const connection = mongoose.connect(url.MONGO_URL_DB_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connect(url.MONGO_URL_NUBE, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connection.on('connected', () => {
-    console.log('[Mongoose] - connected in:', url.MONGO_URL_DB_LOCAL);
+    console.log('[Mongoose] - connected in:', url.MONGO_URL_NUBE);
 });
 
 mongoose.connection.on('error', (err) => {
