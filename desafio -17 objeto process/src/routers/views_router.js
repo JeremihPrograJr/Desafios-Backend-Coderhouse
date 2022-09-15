05a5b9ac284862f113bed02e07bd70a926c4eac5
JeremiasProgJr <48654253+JeremiasProgJr@users.noreply.github.com>
2022-09-15 17:44:47 -0300

    agregando vista info
    .

--- desafio -17 objeto process/src/routers/views_router.js
+++ desafio -17 objeto process/src/routers/views_router.js
@@ -22,6 +22,31 @@ router.get('/login',(req,res) => {
 router.get('/failregister',(req,res) => {
     if(req.session.user) return res.render('error_register')
 })
-
+router.get('/info', (req,res) => {
+    let argumentos= process.argv
+    let sistema = process.platform
+    let version = process.version
+    let memoria = process.memoryUsage()
+    let path = process.execPath
+    let pid = process.pid
+    let carpeta = process.cwd()
+    res.render('info',{
+        argumentos:argumentos,
+        sistema:sistema,
+        version:version,
+        memoria:JSON.stringify(memoria),
+        path:path,
+        pid:pid,
+        carpeta:carpeta
+        })
+        /*
+    console.log('Argumentos de entrada', process.argv);
+    console.log('Sistema operativo', process.platform);
+    console.log('Version node', process.version);
+    console.log('memoria', process.memoryUsage());
+    console.log('PATH de ejecucion ',process.execPath);
+    console.log('PID  ',process.pid);
+        */
+})
 
 module.exports = router
