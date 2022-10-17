dc4e621693ca339b662a3f482f6f53be6127b6e2
JeremiasProgJr <48654253+JeremiasProgJr@users.noreply.github.com>
2022-10-17 20:36:16 -0300

    .
    .

---
+++ desafio-19 Loggers gzip y analisis de performance/src/serverFork.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const { fork } = require('child_process');
+
+
+const server = app.listen((8080) , ()=>{})
+
+
+app.get(('/api/randoms'),(req,res) => {
+    let cantidad =req.query.cantidad
+        if (cantidad === undefined || cantidad === null ){
+            cantidad = 10000000
+        }
+        const computo = fork('./random.js');
+        computo.send(cantidad);
+        computo.on('message', resultado => {
+            res.send(JSON.stringify(resultado));
+        });
+  
+    
+})
+
+app.get(('/info'), (req,res) => {
+    res.send('ok')
+})
