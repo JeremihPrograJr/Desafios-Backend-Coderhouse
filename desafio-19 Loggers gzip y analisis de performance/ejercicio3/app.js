6486a0469f7b6cb8407bca045ea1fe255cf42e95
JeremiasProgJr <48654253+JeremiasProgJr@users.noreply.github.com>
2022-10-17 18:24:45 -0300

    compresion
    compresion

--- desafio-19 Loggers gzip y analisis de performance/ejercicio3/app.js
+++ desafio-19 Loggers gzip y analisis de performance/ejercicio3/app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const pino = require('pino')
 
+const streams = [
+    {level:'debug', stream:process.stdout},
+    {level:'error',stream:pino.destination('./pinoerrors.log')}
+]
 const logger = pino({
 
-}, pino.multistream())
+}, pino.multistream(streams))
 
 
 const server = app.listen(8080,()=>{
@@ -12,5 +16,12 @@ console.log("escuchando server 8080")
 })
 
 app.get(('/'),(req,res) => {
+    logger.fatal('fatal')
+    logger.error('error')
+    logger.warn('warn')
+    logger.info('info')
+    logger.debug('debug')
+    logger.trace('trace')
+
     res.send("listo ahi")
 })
--- desafio-19 Loggers gzip y analisis de performance/src/server.js
+++ desafio-19 Loggers gzip y analisis de performance/src/server.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
+const compression = require('compression')
 
 const server = app.listen((8080), ()=>{
     console.log("Server escuchando puerto 8080")
 
 } )
 
-app.get(('/'),(req,res)=> {
-    res.send("Hola")
+//Comentar para hacer con y sin 
+app.use(compression())
+
+
+app.get(('/infocompresion'),(req,res)=> {
+    let string = "hola como estan"
+    for(let i =0 ; i < 1000; i++){
+        string +=" hola como estan "
+    }
+    res.send(string)
 })
