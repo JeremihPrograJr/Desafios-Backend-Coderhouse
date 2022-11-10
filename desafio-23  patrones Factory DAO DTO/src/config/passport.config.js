d2c67dd7daf74e1196eaa990e63f82df352ceaa5
JeremiasProgJr <48654253+JeremiasProgJr@users.noreply.github.com>
2022-11-10 19:24:31 -0300

    .
    .

---
+++ desafio-23  patrones Factory DAO DTO/src/config/passport.config.js
@@ -0,0 +1,58 @@
+const { compareSync } = require('bcrypt');
+const passport = require('passport')
+const local = require('passport-local')
+const   {usuarioDao}  =require('../daos/index.js');
+const usersService =usuarioDao
+
+const {createHash,isValidPassword}= require('../utils')
+
+const LocalStrategy = local.Strategy    //local  = username +password  o email +password
+
+
+
+const initializedPassport = () => {
+    passport.use('register',new LocalStrategy({passReqToCallback:true,usernameField:'email'},async(req,email,password,done) => {
+        try {
+            const {name,last_name,age,alias,adress,phone,avatar}= req.body 
+            console.log(req.file)
+           
+            
+            if(!email||!name||!last_name|| !phone || !age||!alias|| !password || !adress)return done(null,false)
+            const existe =  await usersService.findEmail({email:email})
+            
+            if(existe) return done(null,false)
+            
+            let objeto = {
+                email,
+                name,
+                last_name,
+                phone,
+                age,
+                adress,
+                alias,
+                avatar:req.file.filename ,
+                password:createHash(password),
+               
+            }
+            let resultado = await usersService.create(objeto) 
+            return done(null,resultado)
+        } catch (error) {
+            return done(error)
+        }
+  
+    }))
+
+    passport.serializeUser((user,done) => {
+        done(null,user._id)
+    })
+
+    passport.deserializeUser( async (id,done) => {
+        let resultado = await usersService.findEmail({_id:id})
+        return done(null,resultado)
+    })
+}
+
+
+
+
+module.exports = initializedPassport
